dab6f2a4a79b0fae4d4c87faa6946829fa0aa911
Alexey Kurylev <chepe4pi@gmail.com>
2024-07-25 16:14:28 -0300

    init project_run

---
+++ project_run/settings/production.py
@@ -0,0 +1,13 @@
+from .base import *
+
+AWS_STORAGE_BUCKET_NAME = 'zappa-ymqd03cou'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_DEFAULT_ACL = None
+
+# Static files (CSS, JavaScript, Images)
+STATIC_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
